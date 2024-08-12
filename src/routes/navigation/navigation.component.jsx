314ec35754abb4f0fa63dafffe3ec9042e2010a6
Santiago H <altosolid@gmail.com>
2024-08-12 09:21:15 -0500

    PW-11: Add whatsapp floating buttin

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,12 +2,26 @@ import { Outlet } from 'react-router-dom';
 import NavBar from '../../components/navBar/navBar.component';
 import Footer from '../../components/footer/footer.component';
 
+import HeaderLogo from '../../assets/logos/header-logo.svg';
+
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
+
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <div className='navigation-container'>
       <NavBar />
+      <FloatingWhatsApp
+        phoneNumber="573172688034"
+        accountName="Pro working"
+        allowEsc
+        allowClickAway
+        avatar={HeaderLogo}
+        statusMessage='Normalmente responde en 1 hora'
+        chatMessage="👋 ¡Hola! Gracias por contactar con Pro Working! Cómo podemos ayudarte en esta ocasión?"
+      />
       <Outlet />
       <Footer />
     </div>
