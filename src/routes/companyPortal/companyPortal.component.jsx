f439984dd71784e3b48c3d0a0de4fb902de683a9
Santiago H <altosolid@gmail.com>
2024-08-12 00:13:44 -0500

    PW-04: Improving responsiveness

--- src/routes/companyPortal/companyPortal.component.jsx
+++ src/routes/companyPortal/companyPortal.component.jsx
@@ -4,7 +4,13 @@ import './companyPortal.styles.scss';
 const CompanyPortal = () => {
   return (
     <div>
-      Company portal
+      <iframe 
+        width='100%' 
+        height='1200px' 
+        scrolling='auto' 
+        src='https://app.biofile.com.co/api/integracionweb/Z3kODT4FgF6D6cBnMykfiLoqUULkMtwP5IqNdSUYDT4=' 
+        frameBorder='0'>
+      </iframe>
     </div>
   );
 }
